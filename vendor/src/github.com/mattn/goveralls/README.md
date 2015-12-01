5583e6042a3cc3891b7a774fbc1a7303a9df243d
Wallace Ricardo <wallace.crd@gmail.com>
2015-12-01 10:26:50 -0500

    Vendor test tools

---
+++ vendor/src/github.com/mattn/goveralls/README.md
@@ -0,0 +1,132 @@
+goveralls
+=========
+
+[Go](http://golang.org) integration for [Coveralls.io](http://coveralls.io)
+continuous code coverage tracking system.
+
+# Installation
+
+`goveralls` requires a working Go installation (Go-1.2 or higher).
+
+```bash
+$ go get github.com/mattn/goveralls
+```
+
+
+# Usage
+
+First you will need an API token.  It is found at the bottom of your
+repository's page when you are logged in to Coveralls.io.  Each repo has its
+own token.
+
+```bash
+$ cd $GOPATH/src/github.com/yourusername/yourpackage
+$ goveralls -repotoken your_repos_coveralls_token
+```
+
+You can set the environment variable `$COVERALLS_TOKEN` to your token so you do
+not have to specify it at each invocation.
+
+# Continuous Integration
+
+There is no need to run `go test` separately, as `goveralls` runs the entire
+test suite.
+
+## Travis CI
+
+### GitHub Integration
+
+Enable Travis-CI on your github repository settings.
+
+For a **public** github repository put below's `.travis.yml`.
+
+```
+language: go
+go:
+  - tip
+before_install:
+  - go get github.com/axw/gocov/gocov
+  - go get github.com/mattn/goveralls
+  - if ! go get github.com/golang/tools/cmd/cover; then go get golang.org/x/tools/cmd/cover; fi
+script:
+    - $HOME/gopath/bin/goveralls -service=travis-ci
+```
+
+For a **private** github repository put below's `.travis.yml`. If you use **travis pro**, you need to specify `-service=travis-pro` instead of `-service=travis-ci`.
+
+```
+language: go
+go:
+  - tip
+before_install:
+  - go get github.com/axw/gocov/gocov
+  - go get github.com/mattn/goveralls
+  - if ! go get github.com/golang/tools/cmd/cover; then go get golang.org/x/tools/cmd/cover; fi
+script:
+    - $HOME/gopath/bin/goveralls
+```
+
+Store your Coveralls API token in `Environment variables`.
+
+```
+COVERALLS_TOKEN = your_token_goes_here
+```
+
+
+### For others:
+
+```
+$ go get golang.org/x/tools/cmd/cover
+$ go get github.com/mattn/goveralls
+$ go test -covermode=count -coverprofile=profile.cov
+$ goveralls -coverprofile=profile.cov -service=travis-ci
+```
+
+## Drone.io
+
+Store your Coveralls API token in `Environment Variables`:
+
+```
+COVERALLS_TOKEN=your_token_goes_here
+```
+
+Replace the `go test` line in your `Commands` with these lines:
+
+```
+$ go get github.com/axw/gocov/gocov
+$ go get github.com/mattn/goveralls
+$ goveralls -service drone.io
+```
+
+`goveralls` automatically use the environment variable `COVERALLS_TOKEN` as the
+default value for `-repotoken`.
+
+You can use the `-v` flag to see verbose output from the test suite:
+
+```
+$ goveralls -v -service drone.io
+```
+
+## CircleCI
+
+Store your Coveralls API token as an [Environment Variable](https://circleci.com/docs/environment-variables).
+
+In your `circle.yml` add the following commands under the `test` section.
+
+```yml
+test:
+  pre:
+    - go get github.com/axw/gocov/gocov
+    - go get github.com/mattn/goveralls
+    - if ! go get github.com/golang/tools/cmd/cover; then go get golang.org/x/tools/cmd/cover; fi
+  override:
+    - go test -v -cover -race -coverprofile=/home/ubuntu/coverage.out
+  post:
+    - /home/ubuntu/bin/goveralls -coverprofile=/home/ubuntu/coverage.out -service=circle-ci -repotoken=$COVERALLS_TOKEN
+```
+
+For more information, See https://coveralls.io/docs/go
+
+# License
+
+under the MIT License: http://mattn.mit-license.org/2013
---
+++ vendor/src/github.com/mattn/goveralls/goveralls.go
@@ -0,0 +1,209 @@
+// Copyright (c) 2013 Yasuhiro Matsumoto, Jason McVetta.
+// This is Free Software,  released under the MIT license.
+// See http://mattn.mit-license.org/2013 for details.
+
+// goveralls is a Go client for Coveralls.io.
+package main
+
+import (
+	_ "crypto/sha512"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/pborman/uuid"
+)
+
+/*
+	https://coveralls.io/docs/api_reference
+*/
+
+var (
+	pkg       = flag.String("package", "", "Go package")
+	verbose   = flag.Bool("v", false, "Pass '-v' argument to 'go test'")
+	gocovjson = flag.String("gocovdata", "", "If supplied, use existing gocov.json")
+	coverprof = flag.String("coverprofile", "", "If supplied, use a go cover profile")
+	covermode = flag.String("covermode", "count", "sent as covermode argument to gocov if applicable")
+	repotoken = flag.String("repotoken", os.Getenv("COVERALLS_TOKEN"), "Repository Token on coveralls")
+	endpoint  = flag.String("endpoint", "https://coveralls.io", "Hostname to submit Coveralls data to")
+	service   = flag.String("service", "travis-ci", "The CI service or other environment in which the test suite was run. ")
+	shallow   = flag.Bool("shallow", false, "Shallow coveralls internal server errors")
+)
+
+// usage supplants package flag's Usage variable
+var usage = func() {
+	cmd := os.Args[0]
+	// fmt.Fprintf(os.Stderr, "Usage of %s:\n", cmd)
+	s := "Usage: %s [options] TOKEN\n"
+	fmt.Fprintf(os.Stderr, s, cmd)
+	flag.PrintDefaults()
+}
+
+// A SourceFile represents a source code file and its coverage data for a
+// single job.
+type SourceFile struct {
+	Name     string        `json:"name"`     // File path of this source file
+	Source   string        `json:"source"`   // Full source code of this file
+	Coverage []interface{} `json:"coverage"` // Requires both nulls and integers
+}
+
+// A Job represents the coverage data from a single run of a test suite.
+type Job struct {
+	RepoToken          *string       `json:"repo_token,omitempty"`
+	ServiceJobId       string        `json:"service_job_id"`
+	ServicePullRequest string        `json:"service_pull_request,omitempty"`
+	ServiceName        string        `json:"service_name"`
+	SourceFiles        []*SourceFile `json:"source_files"`
+	Git                *Git          `json:"git,omitempty"`
+	RunAt              time.Time     `json:"run_at"`
+}
+
+// A Response is returned by the Coveralls.io API.
+type Response struct {
+	Message string `json:"message"`
+	URL     string `json:"url"`
+	Error   bool   `json:"error"`
+}
+
+func getCoverage() []*SourceFile {
+	if *coverprof != "" {
+		return parseCover(*coverprof)
+	} else {
+		return getCoverageGocov()
+	}
+}
+
+var vscDirs = []string{".git", ".hg", ".bzr", ".svn"}
+
+func findRepositoryRoot(dir string) (string, bool) {
+	for _, vcsdir := range vscDirs {
+		if d, err := os.Stat(filepath.Join(dir, vcsdir)); err == nil && d.IsDir() {
+			return dir, true
+		}
+	}
+	nextdir := filepath.Dir(dir)
+	if nextdir == dir {
+		return "", false
+	}
+	return findRepositoryRoot(nextdir)
+}
+
+func getCoverallsSourceFileName(name string) string {
+	if dir, ok := findRepositoryRoot(name); !ok {
+		return name
+	} else {
+		filename := strings.TrimPrefix(name, dir+string(os.PathSeparator))
+		return filename
+	}
+}
+
+func process() error {
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	//
+	// Parse Flags
+	//
+	flag.Usage = usage
+	flag.Parse()
+
+	//
+	// Setup PATH environment variable
+	//
+	paths := filepath.SplitList(os.Getenv("PATH"))
+	if goroot := os.Getenv("GOROOT"); goroot != "" {
+		paths = append(paths, filepath.Join(goroot, "bin"))
+	}
+	if gopath := os.Getenv("GOPATH"); gopath != "" {
+		for _, path := range filepath.SplitList(gopath) {
+			paths = append(paths, filepath.Join(path, "bin"))
+		}
+	}
+	os.Setenv("PATH", strings.Join(paths, string(filepath.ListSeparator)))
+
+	//
+	// Initialize Job
+	//
+	var jobId string
+	if travisJobId := os.Getenv("TRAVIS_JOB_ID"); travisJobId != "" {
+		jobId = travisJobId
+	} else if circleCiJobId := os.Getenv("CIRCLE_BUILD_NUM"); circleCiJobId != "" {
+		jobId = circleCiJobId
+	} else {
+		jobId = uuid.New()
+	}
+	if *repotoken == "" {
+		repotoken = nil // remove the entry from json
+	}
+	var pullRequest string
+	if prNumber := os.Getenv("CIRCLE_PR_NUMBER"); prNumber != "" {
+		// for Circle CI (pull request from forked repo)
+		pullRequest = prNumber
+	} else if prNumber := os.Getenv("TRAVIS_PULL_REQUEST"); prNumber != "" && prNumber != "false" {
+		pullRequest = prNumber
+	} else if prURL := os.Getenv("CI_PULL_REQUEST"); prURL != "" {
+		// for Circle CI
+		pullRequest = regexp.MustCompile(`[0-9]+$`).FindString(prURL)
+	}
+
+	j := Job{
+		RunAt:              time.Now(),
+		RepoToken:          repotoken,
+		ServiceJobId:       jobId,
+		ServicePullRequest: pullRequest,
+		Git:                collectGitInfo(),
+		SourceFiles:        getCoverage(),
+		ServiceName:        *service,
+	}
+
+	b, err := json.Marshal(j)
+	if err != nil {
+		return err
+	}
+
+	params := make(url.Values)
+	params.Set("json", string(b))
+	res, err := http.PostForm(*endpoint+"/api/v1/jobs", params)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("Unable to read response body from coveralls: %s", err)
+	}
+
+	if res.StatusCode >= http.StatusInternalServerError && *shallow {
+		fmt.Println("coveralls server failed internally")
+		return nil
+	}
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("Bad response status from coveralls: %d - %s", res.StatusCode, string(bodyBytes))
+	}
+	var response Response
+	if err = json.Unmarshal(bodyBytes, &response); err != nil {
+		return fmt.Errorf("Unable to unmarshal response JSON from coveralls: %s\n%s", err)
+	}
+	if response.Error {
+		return errors.New(response.Message)
+	}
+	fmt.Println(response.Message)
+	fmt.Println(response.URL)
+	return nil
+}
+
+func main() {
+	if err := process(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
